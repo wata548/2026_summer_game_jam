3681da9af458c02cd2926d9a86b1ed40c36897f6
wata548 <watashimokia@gmail.com>
2026-07-14 09:59:08 +0900

    Fix imprison

---
+++ Assets/Scripts/Entity/StatusEffect/Imprison.cs
@@ -0,0 +1,20 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEditor.Rendering;
+
+namespace StatusEffect
+{
+    internal class Imprison : StatusEffectBase
+    {
+        public Imprison(float pDuration) : base(pDuration){}
+        public override void StartEffect(IEntity pTarget) {
+            pTarget.Movement.MoveLock = true;
+        }
+
+        public override void ExitEffect(IEntity pTarget) {
+            pTarget.Movement.MoveLock = false;
+        }
+    }
+}
