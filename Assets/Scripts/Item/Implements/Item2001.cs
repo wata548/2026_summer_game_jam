08af96dc1f5e2c194363d7170243cdc309b3749e
nearest00 <jinnielovegood08@gmail.com>
2026-07-14 10:33:40 +0900

    Add Item

---
+++ Assets/Scripts/Item/Implements/Item2001.cs
@@ -0,0 +1,19 @@
+﻿using Entity;
+using StatusEffect;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Item
+{
+    internal class Item2001 : ItemBase
+    {
+        public override int Id => 2001;
+        
+        public override void Use()
+        {
+            Player.Instance.Heal(30);
+            Player.Instance.AddStatusEffectBase(new DamageDown(0.2f, 10));
+        }
+    }
+}
---
+++ Assets/Scripts/Item/Implements/Item2003.cs
@@ -0,0 +1,21 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine.Rendering.Universal;
+
+namespace Item
+{
+    internal class Item2003 : ItemBase
+    {
+        public override int Id => 2003;
+        private int usingCount = 0;
+        public override void Use()
+        {
+            if (usingCount == 0)
+            {
+
+            }
+        }
+    }
+}
